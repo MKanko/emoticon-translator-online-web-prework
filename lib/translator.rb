8e13c21aeb873ba0eb4301f34fe4de181be305d2
Mark Kanko <mkanko66@gmail.com>
2019-02-25 03:54:54 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,13 @@ require 'pry'
 
 def load_library(file_path)
   library = YAML.load_file(file_path)
-#binding.pry  
-  library  
+  
+  new_hash = {}
+#  new_array = ["get_emoticon", "get_meaning"]
+
+  new_hash << ["get_emoticon", "get_meaning"]
+binding.pry  
+  
 end
 
 def get_japanese_emoticon
